c09936566d4a9e4a489f99ece9ebc2fcfe2da579
why_so_goofy <64723356+whysogoofy@users.noreply.github.com>
2026-07-18 17:28:01 +0530

    Add: sort-an-array - submission-7

---
+++ Data Structures & Algorithms/sort-an-array/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    # insertion sort
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            key = nums[i]
+            j = i - 1
+
+            while j >= 0 and key < nums[j]:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = key
+        
+        return nums
